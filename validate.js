89205c1f76868d0442d99418abb162fcb5500f98
Pirooz Wallace <PiroozWallace@outlook.com>
2020-11-04 19:59:53 -0800

    adding basic variables
    variables + example

--- validate.js
+++ validate.js
@@ -2,6 +2,18 @@
 
 var apiKey = ba4e9c96082b0721c5c3dbaf01177cf2
 
-var 'http://apilayer.net/api/check?access_key=YOUR_ACCESS_KEY'
+var url = 'http://apilayer.net/api/check?access_key='
 
 var userEmail = ''
+
+
+/*
+
+http://apilayer.net/api/check
+
+    ? access_key = ba4e9c96082b0721c5c3dbaf01177cf2
+    & email = piroozwallace@outlook.com
+    & smtp = 1
+    & format = 1
+
+*/
