f335059285535e3691d238f4b07097c8469139a8
Pirooz Wallace <PiroozWallace@outlook.com>
2020-11-10 20:27:59 -0800

    adding localstorage
    storage object arrays

--- validate.js
+++ validate.js
@@ -12,15 +12,31 @@ var queryURL = url + apiKey + '&email='
 // the user's email
 var userEmail = ''
 
+
 // array of objects that will be used to store user profiles
 var storedUsers = [
   {
      // each user will have email, screenname and password stored
-    email : '',
-    sn : '',
-    pw : ''
+    email : "default@aol.com",
+    sn : 'admin',
+    pw : 'password'
   }
 ]
+
+//var newUser = new storedUsers
+
+// boolean to check if user is unique
+var isUnique = true
+
+// create and set a default user for localStorage
+//var defaultUser = storedUsers
+
+//defaultUser.email = "default@aol.com"
+//defaultUser.sn = "admin"
+//defaultUser.pw = 'password'
+
+// add default user to array
+//storedUsers[0] = defaultUser
 
 /*
  Example api call
@@ -32,12 +48,14 @@ http://apilayer.net/api/check
     & format = 1
 */
 
+// load the users from localStorage file
 function loadUsers()
 {
-  storedUsers = JSON.parse(localStorage.getItem('saveUserArray'))
 
+  storedUsers = JSON.parse(localStorage.getItem('saveUserArray'))
   
 }
+
 
 var form = document.getElementById("registerForm");
 
@@ -56,23 +74,36 @@ function registerEmail()
   // get the user's email
   var emailInput = $('#email').val()
 
+  
   var passwordInput = $('#pw').val()
 
   var usernameInput = $('#sn').val()
 
+  // set to default
+  var newUser = storedUsers
+
+  // make sure there is something stored, otherwise dont check
+  if(storedUsers !== null)
+  {
   for(var i = 0; i < storedUsers.length; i++)
   {
-    if(emailInput === storedUsers.email)
+    // check if email or username is the same
+    if(emailInput === storedUsers[i].email)
     {
       $('#form_results').text('Email already taken')
+      isUnique = false
 
     }
+    else if(usernameInput === storedUsers[i].sn)
+    {
+      $('#form_results').text('Username already taken')
+      isUnique = false
+    }
   }
-
+}
   
   // add it to the query
   queryURL += emailInput
-
 
   // ajax call with mailboxlayer api
 $.ajax({
@@ -89,11 +120,35 @@ $.ajax({
 
     if(response.format_valid && response.mx_found)
     {
-      // debug
+      // debug 
       console.log("email valid")
         
       // display if email is valid
       $('#form_results').text('Email is VALID!')
+
+      // set the new user with inputs if it's unique and valid email
+      if(isUnique)
+      {
+        // store the new variables in the new user object
+        newUser[0].email = emailInput
+         newUser[0].pw = passwordInput
+         newUser[0].sn = usernameInput
+
+         console.log(newUser)
+
+         // add user to array of objects
+        storedUsers.push(newUser[0])
+
+        // add it localstorage
+        localStorage.setItem('saveUserArray', JSON.stringify(storedUsers))
+
+        $('<p>').text()
+
+        // debug console
+        console.log(storedUsers)
+  
+      }
+
         console.log("format")
     }
     else
