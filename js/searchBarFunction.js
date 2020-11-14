4e2799bc796a5783665ae9aa40410eb4213e2f73
Pirooz Wallace <PiroozWallace@outlook.com>
2020-11-14 13:25:21 -0800

    result grid
    ui kit card results grid

--- js/searchBarFunction.js
+++ js/searchBarFunction.js
@@ -33,53 +33,76 @@ var searchFunction = function (event) {
                 var thisResult = json._embedded.events[i]
                  
                 // STARTING TO DISPLAY RESULTS AS CARD  -- should eventually be styled better
-                var card = $("<div>").addClass("card");
+                var card = $("<div>").addClass("card")
 
                 // MAKE ROWS AND COLUMNS
-                card.addClass('.row')
-                card.addClass('.col-4')
+               // card.addClass('.row')
+               // card.addClass('.col-4')
 
                 var cardBody = $("<div>").addClass("card-body");
                 var eventTitle = $("<h4>").addClass("card-title").text(thisResult.name);
 
                 // get the hyperlink to buy tickets for event
                 var link = $("<a>").attr("href", thisResult.url);
+               // link.css('')
+        
                 
                 // set the text for the link
                 link.text(thisResult.name)
 
+             //  link.addClass("card-title")
+
+                var blankline = $('<p>').text()
+
                 // debug console
                 console.log(link)
+
+                var linkText = $('<h2>').append(link)
 
                 // event image
                 var image = $("<img>").attr("src", json._embedded.events[i].images[0].url)
 
+                // responsive images
+                image.addClass('img-fluid')
+
+              // change the image height and width
+                image.height(200)
+                image.width(300)
+
                 // venue
                 var venue = $('<p>').addClass('card-text').text("Venue:  " + thisResult._embedded.venues[0].name )
 
-                // price range
 
                 // make sure price range exists
                 if(thisResult.priceRanges)
                 {
-                var priceText = $('<p>').addClass('card-text').text("Price range: $" + thisResult.priceRanges[0].min 
-                + '  to:  $' + thisResult.priceRanges[0].max)
+                
+                    // get the price range for tickets
+                    var priceText = $('<p>').addClass('card-text').text("Price range: $" + thisResult.priceRanges[0].min 
+                    + '  to:  $' + thisResult.priceRanges[0].max)
 
-                       // add the content to the cardBody
-                       cardBody.append(eventTitle, link, image, priceText, venue)
+                    // add the content to the cardBody
+                    cardBody.append(linkText, image, priceText, venue)
                 }
                 else{
-                    cardBody.append(eventTitle, link, image,venue )
+                    cardBody.append(linkText, image, venue )
                 }
+                
 
-            
-         
-    
+                cardBody.css('border-color', 'yellow')
+                
                 // add the cardBody to the card
                 card.append(cardBody);
+
+                // put a border on the cards
+                card.css('border', 'solid 4px black');
+
+                card.addClass('uk-card uk-card-default')
+                
                 
                 // add the card to the page
                 $('#searchCards').append(card)
+              //  $('#searchCards').show(1000)
             }
         }
         else
@@ -95,8 +118,7 @@ var searchFunction = function (event) {
         },
         error: function (xhr, status, err) {
             // This time, we do not end up here!
-
-            
+        
         }
     });
 }
