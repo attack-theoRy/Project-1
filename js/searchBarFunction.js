2c2ee7fd09492f92a506c607958ba8a77d8f550f
Pirooz Wallace <PiroozWallace@outlook.com>
2020-11-18 16:24:21 -0800

    Next and Previous pages!!!!
    dissected the object some more to get full results! Added buttons for next and previous pages! Now we actually see everything instead of just like 15 results out of a 100

--- js/searchBarFunction.js
+++ js/searchBarFunction.js
@@ -1,5 +1,10 @@
+// Global Pages
+
+var currentPage = 0 
+var totalPages = 0
 
 
+// main search function
 var searchFunction = function (event) {
     event.preventDefault()
     
@@ -13,16 +18,35 @@ var searchFunction = function (event) {
     var city = "&city=" + document.querySelector('#searchCity').value
     var stateCode = "&stateCode=" + document.querySelector('#searchState').value
    // var countryCode = '&countryCode=' + document.querySelector('#searchCountry').value
-    var startDate = '&startDateTime=' + document.querySelector('#startDate').value + 'T00:00:00Z'
+   
+   // set the variable for input dates
+   var inputStartDate = document.querySelector('#startDate').value
+   
+   var inputEndDate = document.querySelector('#endDate').value
 
-    var endDate = '&endDateTime=' + document.querySelector('#endDate').value + 'T00:00:00Z'
+   var startDate = ''
+   var endDate = ''
+   
+   // only add value if user specified date
+   if(inputStartDate !== '')
+   {
+        startDate = '&startDateTime=' + document.querySelector('#startDate').value + 'T00:00:00Z'
+   }
+
+
+   // only add value if user specified date
+   if(inputEndDate !== '')
+   {
+       endDate = '&endDateTime=' + document.querySelector('#endDate').value + 'T00:00:00Z'
+   }
+
+
     var query = "&apikey=rjC9JcYGVmI9QLKslEzKTEDnb93gABPp"
     var locale = "&locale=*"
 
     console.log('debugHere')
-   // console.log(document.querySelector('#startDate').value)
-   // console.log(document.querySelector('#endDate').value)
 
+    
     $.ajax({
         type: "GET",
         url: queryURL + locale + searchEvent + searchClassification + startDate + endDate + sortFilter + city + stateCode + query,
@@ -44,6 +68,14 @@ var searchFunction = function (event) {
 
             // clear results 
             $('#searchResults').text('')
+
+
+            // reset these values after search submission
+            totalPages = json.page.totalPages
+            currentPage = 0
+
+            console.log('Total pages = ' + totalPages)
+
 
             if(json.page.totalElements > 0)
             {
@@ -178,6 +210,419 @@ var searchFunction = function (event) {
 // event listener for form submission
 $('#searchBar').submit(searchFunction)
 
+$('#nextButton').on('click', nextPage)
+
+$('#prevButton').on('click', prevPage)
+
+// getting the next page of results
+function nextPage()
+{
+    currentPage++
+
+    console.log(currentPage)
+    console.log
+    
+    if(currentPage < totalPages)
+
+        // filter by page param
+        var pageDivider = '&page=' + currentPage
+
+        var queryURL = "https://app.ticketmaster.com/discovery/v2/events?"
+        var searchEvent = "&keyword=" + document.querySelector('#searchEvent').value
+    
+        var searchClassification = '&classificationName=' + document.querySelector('#classification').value
+    
+        var sortFilter = '&sort=' + document.querySelector('#sort').value
+        
+        var city = "&city=" + document.querySelector('#searchCity').value
+        var stateCode = "&stateCode=" + document.querySelector('#searchState').value
+       // var countryCode = '&countryCode=' + document.querySelector('#searchCountry').value
+       
+       // set the variable for input dates
+       var inputStartDate = document.querySelector('#startDate').value
+       
+       var inputEndDate = document.querySelector('#endDate').value
+    
+       var startDate = ''
+       var endDate = ''
+       
+       // only add value if user specified date
+       if(inputStartDate !== '')
+       {
+            startDate = '&startDateTime=' + document.querySelector('#startDate').value + 'T00:00:00Z'
+       }
+    
+    
+       // only add value if user specified date
+       if(inputEndDate !== '')
+       {
+           endDate = '&endDateTime=' + document.querySelector('#endDate').value + 'T00:00:00Z'
+       }
+    
+    
+        var query = "&apikey=rjC9JcYGVmI9QLKslEzKTEDnb93gABPp"
+        var locale = "&locale=*"
+    
+        console.log('NextPage api call')
+
+        $.ajax({
+            type: "GET",
+            url: queryURL + locale + searchEvent + searchClassification + startDate + endDate + sortFilter + city + stateCode + query + pageDivider,
+            async: true,
+            dataType: "json",
+            success: function (json) {
+
+                           // empty the search results first
+            $('#searchCards').empty()
+
+            //console.log('Total request' + url)
+
+            console.log(city)
+            console.log(stateCode)
+            console.log(json)
+
+            // clear results 
+            $('#searchResults').text('')
+
+
+            if(json.page.totalElements > 0)
+            {
+                console.log(json)
+            // loop through all results
+            for (var i = 0; i < json._embedded.events.length; i++) {
+                
+                // console debug
+                console.log(json._embedded.events[i])
+
+
+                // to abbreviate the calls
+                var thisResult = json._embedded.events[i]
+                 
+                // STARTING TO DISPLAY RESULTS AS CARD  -- should eventually be styled better
+                var card = $("<div>").addClass("uk-card-secondary")
+
+
+                var cardBody = $("<div>").addClass("uk-card-body");
+
+                //var eventTitle = $("<h4>").addClass("uk-card-title").text(thisResult.name);
+
+                // get the hyperlink to buy tickets for event
+                var link = $("<a>").attr("href", thisResult.url);
+
+                // make the link the card title
+                link.addClass('uk-card-title')
+
+                
+                // set the text for the link
+                link.text(thisResult.name)
+
+             
+                // dates of the concert
+                if(thisResult.dates.start.localTime)
+                {
+                var dates = $('<p>').text('Date / Time : ' + thisResult.dates.start.localDate + ' at ' + thisResult.dates.start.localTime)
+                dates.css({"font-family": "Garamond, bold", 'color': 'red', 'font-size' : '120%'} )
+            }
+            else{
+                var dates = $('<p>').text('Date / Time : ' + thisResult.dates.start.localDate + ' time TBA' )
+                dates.css({"font-family": "Garamond, bold", "color" : "red", "font-size": "120%"} )
+
+            }
+
+
+                // debug console
+                console.log(link)
+
+                // put the link on a separate line
+                var linkText = $('<h2>').append(link)
+
+                // event image
+                var image = $("<img>").attr("src", json._embedded.events[i].images[0].url)
+
+                // responsive images
+                image.addClass('img-fluid')
+
+              // change the image height and width
+                image.height(200)
+                image.width(300)
+
+                // venue
+                var venue = $('<p>').addClass('card-text').text("Venue:  " + thisResult._embedded.venues[0].name )
+
+                venue.css({'color': 'white'})
+                venue.css({"font-family": "Garamond, bold", "font-size": "150%"} )
+
+                var venueAddress = $('<p>').addClass('card-text').text("Address: " + thisResult._embedded.venues[0].address.line1)
+
+
+                // make sure price range exists
+                if(thisResult.priceRanges)
+                {
+
+                
+                    // get the price range for tickets
+                    var priceText = $('<p>').addClass('card-text').text("Price range: $" + thisResult.priceRanges[0].min 
+                    + '  to:  $' + thisResult.priceRanges[0].max)
+
+                    // change text to green
+                    priceText.css({'color': 'green'})
+
+                    // add the content to the cardBody
+                    cardBody.append(linkText, image, dates, priceText, venue, venueAddress)
+                }
+                else{
+
+                    var priceText = $('<p>').addClass('card-text').text('Price range: TBA')
+                    
+                    // change text to green
+                    priceText.css({'color': 'green'})
+
+                    cardBody.append(linkText, image, dates, priceText, venue, venueAddress )
+                }
+                
+
+                
+                // add the cardBody to the card
+                card.append(cardBody);
+
+                // put a border on the cards
+                card.css('border', 'solid 4px black');
+
+                // add default styling to result cards
+                card.addClass('uk-card uk-card-default')
+                
+                
+                // add the card to the page
+                $('#searchCards').append(card)
+              //  $('#searchCards').show(1000)
+            }
+        }
+        else
+        {
+            // error checking display  --- feel free to style differently
+            $('#searchResults').text('No results with that criteria')   
+            $('#searchResults').css({'color':'white'})
+            $('#searchResults').css({'background-color':'black'})
+            console.log('No results with that criteria')
+        }
+
+    },
+    error: function (xhr, status, err) {
+        // This time, we do not end up here!
+    
+    }
+
+            
+        });
+
+    }
+
+
+    // getting the next page of results
+function prevPage()
+{
+    currentPage--
+
+    console.log(currentPage)
+    
+    
+    if(currentPage >= 0)
+
+        // filter by page param
+        var pageDivider = '&page=' + currentPage
+
+        var queryURL = "https://app.ticketmaster.com/discovery/v2/events?"
+        var searchEvent = "&keyword=" + document.querySelector('#searchEvent').value
+    
+        var searchClassification = '&classificationName=' + document.querySelector('#classification').value
+    
+        var sortFilter = '&sort=' + document.querySelector('#sort').value
+        
+        var city = "&city=" + document.querySelector('#searchCity').value
+        var stateCode = "&stateCode=" + document.querySelector('#searchState').value
+       // var countryCode = '&countryCode=' + document.querySelector('#searchCountry').value
+       
+       // set the variable for input dates
+       var inputStartDate = document.querySelector('#startDate').value
+       
+       var inputEndDate = document.querySelector('#endDate').value
+    
+       var startDate = ''
+       var endDate = ''
+       
+       // only add value if user specified date
+       if(inputStartDate !== '')
+       {
+            startDate = '&startDateTime=' + document.querySelector('#startDate').value + 'T00:00:00Z'
+       }
+    
+    
+       // only add value if user specified date
+       if(inputEndDate !== '')
+       {
+           endDate = '&endDateTime=' + document.querySelector('#endDate').value + 'T00:00:00Z'
+       }
+    
+    
+        var query = "&apikey=rjC9JcYGVmI9QLKslEzKTEDnb93gABPp"
+        var locale = "&locale=*"
+    
+        console.log('NextPage api call')
+
+        $.ajax({
+            type: "GET",
+            url: queryURL + locale + searchEvent + searchClassification + startDate + endDate + sortFilter + city + stateCode + query + pageDivider,
+            async: true,
+            dataType: "json",
+            success: function (json) {
+
+                           // empty the search results first
+            $('#searchCards').empty()
+
+            //console.log('Total request' + url)
+
+            console.log(city)
+            console.log(stateCode)
+            console.log(json)
+
+            // clear results 
+            $('#searchResults').text('')
+
+
+            if(json.page.totalElements > 0)
+            {
+                console.log(json)
+            // loop through all results
+            for (var i = 0; i < json._embedded.events.length; i++) {
+                
+                // console debug
+                console.log(json._embedded.events[i])
+
+
+                // to abbreviate the calls
+                var thisResult = json._embedded.events[i]
+                 
+                // STARTING TO DISPLAY RESULTS AS CARD  -- should eventually be styled better
+                var card = $("<div>").addClass("uk-card-secondary")
+
+
+                var cardBody = $("<div>").addClass("uk-card-body");
+
+                //var eventTitle = $("<h4>").addClass("uk-card-title").text(thisResult.name);
+
+                // get the hyperlink to buy tickets for event
+                var link = $("<a>").attr("href", thisResult.url);
+
+                // make the link the card title
+                link.addClass('uk-card-title')
+
+                
+                // set the text for the link
+                link.text(thisResult.name)
+
+             
+                // dates of the concert
+                if(thisResult.dates.start.localTime)
+                {
+                var dates = $('<p>').text('Date / Time : ' + thisResult.dates.start.localDate + ' at ' + thisResult.dates.start.localTime)
+                dates.css({"font-family": "Garamond, bold", 'color': 'red', 'font-size' : '120%'} )
+            }
+            else{
+                var dates = $('<p>').text('Date / Time : ' + thisResult.dates.start.localDate + ' time TBA' )
+                dates.css({"font-family": "Garamond, bold", "color" : "red", "font-size": "120%"} )
+
+            }
+
+
+                // debug console
+                console.log(link)
+
+                // put the link on a separate line
+                var linkText = $('<h2>').append(link)
+
+                // event image
+                var image = $("<img>").attr("src", json._embedded.events[i].images[0].url)
+
+                // responsive images
+                image.addClass('img-fluid')
+
+              // change the image height and width
+                image.height(200)
+                image.width(300)
+
+                // venue
+                var venue = $('<p>').addClass('card-text').text("Venue:  " + thisResult._embedded.venues[0].name )
+
+                venue.css({'color': 'white'})
+                venue.css({"font-family": "Garamond, bold", "font-size": "150%"} )
+
+                var venueAddress = $('<p>').addClass('card-text').text("Address: " + thisResult._embedded.venues[0].address.line1)
+
+
+                // make sure price range exists
+                if(thisResult.priceRanges)
+                {
+
+                
+                    // get the price range for tickets
+                    var priceText = $('<p>').addClass('card-text').text("Price range: $" + thisResult.priceRanges[0].min 
+                    + '  to:  $' + thisResult.priceRanges[0].max)
+
+                    // change text to green
+                    priceText.css({'color': 'green'})
+
+                    // add the content to the cardBody
+                    cardBody.append(linkText, image, dates, priceText, venue, venueAddress)
+                }
+                else{
+
+                    var priceText = $('<p>').addClass('card-text').text('Price range: TBA')
+                    
+                    // change text to green
+                    priceText.css({'color': 'green'})
+
+                    cardBody.append(linkText, image, dates, priceText, venue, venueAddress )
+                }
+                
+
+                
+                // add the cardBody to the card
+                card.append(cardBody);
+
+                // put a border on the cards
+                card.css('border', 'solid 4px black');
+
+                // add default styling to result cards
+                card.addClass('uk-card uk-card-default')
+                
+                
+                // add the card to the page
+                $('#searchCards').append(card)
+              //  $('#searchCards').show(1000)
+            }
+        }
+        else
+        {
+            // error checking display  --- feel free to style differently
+            $('#searchResults').text('No results with that criteria')   
+            $('#searchResults').css({'color':'white'})
+            $('#searchResults').css({'background-color':'black'})
+            console.log('No results with that criteria')
+        }
+
+    },
+    error: function (xhr, status, err) {
+        // This time, we do not end up here!
+    
+    }
+
+            
+        });
+
+    }
+
+
+
 // displays results from home city
 function showHomeResults(homeCity, homeState)
 {
@@ -281,8 +726,7 @@ function showHomeResults(homeCity, homeState)
                         cardBody.append(linkText, image, dates, venue )
                     }
                     
-    
-                    
+             
                     // add the cardBody to the card
                     card.append(cardBody);
     
